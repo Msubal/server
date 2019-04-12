1e7d688dae7f4da7b93bdf73eb3424a2f5bfffa8
Msubal <45891823+Msubal@users.noreply.github.com>
2019-04-12 18:21:47 +0900

    json 파싱완료

--- adminSystem2/src/main/java/kr/ac/hansung/model/Student.java
+++ adminSystem2/src/main/java/kr/ac/hansung/model/Student.java
@@ -1,5 +1,7 @@
 package kr.ac.hansung.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,15 +14,19 @@ public class Student {
 	private int num;
 	
 	//@Min(value=0, message="The Student id must not be less than zero")
+	@JsonProperty("id")
 	private int id;
 	
 	//@Min(value=0, message="The Student password must not be less than zero")
+	@JsonProperty("password")
 	private int password;
 	
 	//@NotEmpty(message="The Student name must not be null")
+	@JsonProperty("name")
 	private String name;
 	
 	//@NotEmpty(message="The Student check must not be null")
+	@JsonProperty("check")
 	private int check;
 	
 }
